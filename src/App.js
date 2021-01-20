c20c0beef2c0768f4cbbc5225d524bfae0cf5e63
tinio.johnchristian@gmail.com <tinio.johnchristian@gmail.com>
2021-01-20 22:06:45 +0900

    PL: Added moneybook branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import './App.css'
+const MoneyBook = () => {
+  const books = [
+    { date: '1/1', item: 'お年玉', amount: 10000 },
+    { date: '1/3', item: 'ケーキ', amount: -500 },
+    { date: '2/1', item: '小遣い', amount: 3000 },
+    { date: '2/5', item: 'マンガ', amount: -600 }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title>小遣い帳</Title>
+      <table className='book'>
+        <thead>
+          <tr>
+            <th>日付</th>
+            <th>項目</th>
+            <th>入金</th>
+            <th>出金</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map(book => (
+            <MoneyBookItem book={book} key={book.date + book.item} />
+          ))}
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
-
-export default App;
+const MoneyBookItem = props => {
+  const { date, item, amount } = props.book
+  return (
+    <tr>
+      <td>date</td>
+      <td>item</td>
+      <td>{amount >= 0 ? amount : null}</td>
+      <td>{amount < 0 ? -amount : null}</td>
+    </tr>
+  )
+}
+MoneyBookItem.propTypes = {
+  book: PropTypes.object.isRequired
+}
+const Title = props => {
+  return <h1>{props.children}</h1>
+}
+Title.propTypes = {
+  children: PropTypes.string
+}
+ReactDOM.render(<MoneyBook />, document.getElementById('root'))
+export default MoneyBook
