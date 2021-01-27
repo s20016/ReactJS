39f1088e641f7c94ac8740e3ca7bf34fc85f4d30
tinio.johnchristian@gmail.com <tinio.johnchristian@gmail.com>
2021-01-27 15:34:29 +0900

    Add: BMI Calculator

--- src/App.js
+++ src/App.js
@@ -1,72 +1,55 @@
 import React from 'react'
 import './App.css'
 
-export default class App2 extends React.Component {
-
+export default class App extends React.Component {
   constructor(props) {
     super(props)
-
     this.state = {
-      jpyVal: '',
-      usdVal: '',
-      phpVal: '',
-      data: []
+      title: 'BMI',
+      userWeight: '',
+      userHeight: '',
+      userBMI: '0'
     }
-
-    // Handlers
-    this.handleJPY = this.handleJPY.bind(this)
-    this.handleUSD = this.handleUSD.bind(this)
-    this.handlePHP = this.handlePHP.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  async componentDidMount() {
-    const url = 'http://localhost:3000/ReactJS/rates.json'
-    window.fetch(url).then(res => res.json()).then(json => this.setState({ data: json}))
-  }
-
-  // JPY
-  handleJPY = (event) => {
+  handleHeight = (event) => {
     const value = event.target.value
-    const data = this.state.data.JPY
     this.setState({
-      jpyVal: value,
-      usdVal: value * data.USD,
-      phpVal: value * data.PHP
+      userHeight: value
     })
   }
 
-  // USD
-  handleUSD = (event) => {
+  handleWeight = (event) => {
     const value = event.target.value
-    const data = this.state.data.USD
     this.setState({
-      usdVal: value,
-      jpyVal: value * data.JPY,
-      phpVal: value * data.PHP
+      userWeight: value
     })
   }
 
-  // PHP
-  handlePHP = (event) => {
-    const value = event.target.value
-    const data = this.state.data.PHP
+  handleSubmit = (event) => {
+    // BMI = kg/m2
     this.setState({
-      phpVal: value,
-      jpyVal: value * data.JPY,
-      usdVal: value * data.USD
+      userBMI: this.state.userWeight / ((this.state.userHeight / 100) ^ 2),
     })
+    console.log(this.state.userBMI)
   }
 
   render() {
     return (
       <>
-        <h1>{this.state.data.title}</h1>
-        <div>
-          <input type='text' value={this.state.jpyVal} onChange={this.handleJPY}/> ¥<br></br>
-          <input type='text' value={this.state.usdVal} onChange={this.handleUSD}/> $<br></br>
-          <input type='text' value={this.state.phpVal} onChange={this.handlePHP}/> ₱<br></br>
-        </div>
+        <h1>{this.state.title}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <p>Height (cm):</p>
+          <input type='number' value={this.state.userHeight} onChange={this.handleHeight}/><br></br>
+          <p>Weight (kg):</p>
+          <input type='number' value={this.state.userWeight} onChange={this.handleWeight}/><br></br>
+          <button type='button' onClick={this.handleSubmit}>Calculate BMI</button>
+        </form>
+        <p>BMI: {this.state.userBMI}</p>
       </>
     )
   }
+
+
 }
